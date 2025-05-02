efb89ef2a4a8db8c6b5c6e33c0b54c0492871d81
Hadiyatulloh <hadiyatullohfayzullayev@gmail.com>
2025-05-02 16:38:21 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React JS</h1>
+      <h2>This is my first React App</h2>
+
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+
+      <h3>What is React?</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt
+        consectetur, nisi nisl aliquet nunc, vitae porta elit nisl at nulla. Fusce nec purus vitae
+        ligula pulvinar fermentum. Etiam nec erat a magna varius gravida. Nulla facilisi. Aenean
+        vel sem at ante pretium fermentum. Vestibulum id justo a nulla imperdiet suscipit. Curabitur
+        tincidunt sapien sed mi facilisis, non scelerisque nunc ornare. Donec ac purus non quam
+        hendrerit hendrerit. Nam eu magna vel tortor convallis fermentum. Nullam vel turpis non erat
+        porta varius. Aliquam erat volutpat. Quisque nec neque ac odio congue ullamcorper.
+      </p>
+
+      <h3>What is Single Page Application (SPA)?</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lobortis massa sit amet nisl
+        lacinia, et tincidunt odio tincidunt. Donec porta sagittis erat, ut luctus sem ultrices
+        vitae. Mauris ut sem vitae sapien mollis vulputate. Integer sed convallis tortor. Vivamus
+        nec dictum magna. Sed laoreet est sed tortor fringilla gravida. Integer eget erat vitae elit
+        facilisis gravida. Suspendisse potenti. Nam pretium congue ligula, nec mattis erat pharetra
+        sed. Sed in erat ac lorem rhoncus aliquam. Vestibulum ante ipsum primis in faucibus orci
+        luctus et ultrices posuere cubilia curae; Mauris convallis erat id turpis tincidunt
+        tincidunt. In nec elit a nisi accumsan laoreet. Aenean mattis orci eget nisi finibus, sed
+        blandit lectus faucibus. Curabitur luctus tincidunt sem vel rhoncus. Ut ut finibus lorem.
+      </p>
+    </div>
+  );
+}
+
+export default App;
